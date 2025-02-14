9d8b75e7789f7247de221c4e8cc7b402dd1af0f9
robotgoat <robotgoat@users.noreply.github.com>
2025-02-13 21:25:18 -0500

    change to typescript; create basic homepage and theming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import ThemedImage from '@theme/ThemedImage'
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -12,10 +15,19 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
+        {/* <Heading as="h1" className="hero__title">
           {siteConfig.title}
-        </Heading>
+        </Heading> */}
+        <ThemedImage
+          alt="INAV"
+          className='h-fit w-fit xl:mr-12'
+          sources={{
+            light: useBaseUrl('img/inav_home_light.svg'),
+            dark: useBaseUrl('img/inav_home_dark.svg')
+          }}
+        />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="xs:text-sm sm:text-lg xl:text-xl">INAV is a cutting edge flight controller software whose <br></br> focus is on bringing semi-autonomous flight capabilities <br></br> for fixed wing, rotary wing, and VTOL remote controlled FPV aircraft.</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -28,11 +40,11 @@ function HomepageHeader() {
   );
 }
 
-export default function Home() {
+export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Welcome to ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
