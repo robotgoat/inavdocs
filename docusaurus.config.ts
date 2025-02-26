262bef55d2b4572e16bc17f44df821c7b9d6d087
robotgoat <robotgoat@users.noreply.github.com>
2025-02-25 21:29:25 -0500

    fix borken links?

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
           items: [
             {
               label: "Documentation",
-              to: "/docs/intro",
+              to: "/docs/welcome",
             },
           ],
         },
